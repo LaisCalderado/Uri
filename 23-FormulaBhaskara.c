8e84e21636ed471bd74c521ff4c0197ab5aca5f8
LaisC06 <laiscalderaro06@gmail.com>
2020-07-29 17:31:46 -0400

    Formula de Bhaskara

---
+++ 23-FormulaBhaskara.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+
+    double A, B, C, R1, R2, Delta;
+
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    scanf("%lf", &C);
+
+    Delta = ((B * B) - (4 * A * C));
+    R1 = ((-B) + sqrt(Delta)) / (2 * A);
+    R2 = ((-B) - sqrt(Delta)) / (2 * A);
+
+    if (A != 0 && Delta >= 0){
+        printf("R1 = %.5lf\n", R1);
+        printf("R2 = %.5lf\n", R2);
+    }else{
+        printf("Impossivel calcular\n");
+
+    }
+     
+
+    return 0;
+}
