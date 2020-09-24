68c765d057b722d1b25fed8b8c0708a12fea2786
LaisC06 <laiscalderaro06@gmail.com>
2020-09-24 14:55:09 -0400

    Verificar quais num estão dentro ou fora do intervalo

---
+++ 48-intervalo2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int i, n, x, dentro = 0, fora = 0;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++){
+        scanf("%d", &x);
+
+        if (x >= 10 && x <= 20){
+            dentro++;
+        }
+        else fora++;
+        
+    }
+
+    printf("%d in\n%d out\n", dentro, fora);
+
+
+
+    return 0;
+}
